dc2208b76e2ee386ef337e7cf8c3cc08b942dd2d
Dave Hoy <hoyski@gmail.com>
2020-04-26 08:02:55 -0400

    Initial commit of source from start.spring.io

---
+++ src/main/java/com/hoyski/wordfinderservice/WordFinderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hoyski.wordfinderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordFinderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordFinderServiceApplication.class, args);
+	}
+
+}
