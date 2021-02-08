d7c33f2e875ab63656f8ed614ad65fa4ffb16df3
Dave Hoy <hoyski@gmail.com>
2021-02-08 06:35:59 -0500

    Changed from Spring Boot JAR to WAR for deployment to Tomcat

--- src/main/java/com/hoyski/wordfinderservice/WordFinderServiceApplication.java
+++ src/main/java/com/hoyski/wordfinderservice/WordFinderServiceApplication.java
@@ -2,9 +2,10 @@ package com.hoyski.wordfinderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WordFinderServiceApplication {
+public class WordFinderServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WordFinderServiceApplication.class, args);
